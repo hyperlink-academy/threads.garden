fc298e6a2ddc20442b0228adbca58921e5b61721
Brendan Schlagel <brendan.schlagel@gmail.com>
2022-12-04 20:06:20 -0500

    color tweaks to homepage for better contrast

--- src/routes/index_route.ts
+++ src/routes/index_route.ts
@@ -9,19 +9,15 @@ export const index_route: Route = {
     return new Response(
       html({ head: h("title", "threads.garden"), token: auth }, [
         h("p", { style: "color: green; padding-left: 3%;" }, "—=-_-˜¯-_-≈-"),
+        h("p", { style: "color: #00851d; padding-left:6%;" }, "a small site"),
         h(
           "p",
-          { style: "color: forestgreen; padding-left:6%;" },
-          "a small site"
-        ),
-        h(
-          "p",
-          { style: "color: yellowgreen; padding-left:9%;" },
+          { style: "color: #597f00; padding-left:9%;" },
           "for making threads"
         ),
         h(
           "p",
-          { style: "color: mediumseagreen; padding-left:12%;" },
+          { style: "color: #028550; padding-left:12%;" },
           "on the internet"
         ),
         h(
@@ -33,7 +29,7 @@ export const index_route: Route = {
           "div",
           {
             style:
-              "padding: 1em; background: khaki; border-radius: 8px; margin: 0 16% 1em 20%;",
+              "padding: 1em; background: #f7eea9; border-radius: 8px; margin: 0 16% 1em 20%;",
           },
           [
             h("h2", "start a thread:"),
@@ -48,7 +44,7 @@ export const index_route: Route = {
           "div",
           {
             style:
-              "padding: 1em; background: palegoldenrod; border-radius: 8px; margin: 0 8% 1em 25%;;",
+              "padding: 1em; background: #d6edba; border-radius: 8px; margin: 0 8% 1em 25%;;",
           },
           [
             h("h2", "subscribe to threads:"),
@@ -63,7 +59,7 @@ export const index_route: Route = {
           "div",
           {
             style:
-              "padding: 1em; background: yellowgreen; border-radius: 8px; margin: 0 0 1em 30%;",
+              "padding: 1em; background: #caec85; border-radius: 8px; margin: 0 0 1em 30%;",
           },
           [h("p", "~reply soon — threads close after 7 days!~")]
         ),
